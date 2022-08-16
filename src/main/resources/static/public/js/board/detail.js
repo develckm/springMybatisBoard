c8f7540948f4dc1f7a9972386d5538a36d7acfa8
최경민 <artistckm@gmail.com>
2022-08-16 09:57:08 +0900

    reply 좋아요 싫어요 실패시 undefind 추가되는 현상 해결

--- src/main/resources/static/public/js/board/detail.js
+++ src/main/resources/static/public/js/board/detail.js
@@ -1,5 +1,5 @@
 
-function replyPreferHandler(replyNo,preferActive,btn){
+async function replyPreferHandler(replyNo,preferActive,btn){
 	console.log(preferActive);
 	let url="/reply/prefer/";
 	let prefer=((btn=="good")?true:false);
@@ -15,17 +15,18 @@ function replyPreferHandler(replyNo,preferActive,btn){
 		url+=replyNo+"/"+prefer;
 		method="put";
 	}
-	fetch(url,{method:method})
-		.then((res)=>{
-			if(res.status==200){
-				return res.text();
-			}else if(res.status==400){
-				alert("로그인 하세요!");
-			}else{
-				alert("잘못된 시도입니다.");
-			}
-			
-		})
-		.then((text)=>{document.getElementById(replyLiId).innerHTML=text})
-		.catch((err)=>{alert('다시 시도하세요!'+err)});
+	try{
+		let res=await fetch(url,{method:method});
+		if(res.status==200){
+			let htmlText=await res.text();
+			document.getElementById(replyLiId).innerHTML=htmlText;
+		}else if(res.status==400){
+			alert("로그인 하세요!");
+	
+		}else{
+			alert("잘못된 시도입니다.");
+		}
+	}catch(err){
+		alert('다시 시도하세요!'+err);
+	}
 }
